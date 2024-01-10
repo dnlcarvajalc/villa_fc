44e0f2469897706f8aaa38de4d6103aa58ae7be4
Daniel Carvajal <dnlcarvajalc@gmail.com>
2024-01-10 17:23:51 -0500

    add documentation to read excel function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 def read_excel(file_path:str):
+    """Funcion encargada de leer los datos del archivo excel almacenado.
+
+    Args:
+        file_path (str): la ruta del archivo excel
+
+    Returns:
+        pd.DataFrame: El dataframe completo con la informacion
+    """
     df = pd.read_excel(file_path)
     print(df)
     return df
