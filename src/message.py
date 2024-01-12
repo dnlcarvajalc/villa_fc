031452e45aed84624cba942c195985bcfd822793
Manuela <manuelacorrealopez11@gmail.com>
2024-01-12 17:08:50 -0500

    Envío mensaje de cobro

--- src/message.py
+++ src/message.py
@@ -1,34 +1,22 @@
-import os
 import pywhatkit as kit
 from datetime import datetime
 
-try:
-    diccionario_deudores = {
-        "Jacobo Montenegro Angel": "3233073789",
-        "Juan Jose Gomez": "3165326067",
-        "Daniel Carvajal Correa": "3193917279",
-        "Manuela Correa Lopez": "3185957386"
-    }
+def enviar_mensajes(diccionario_deudores):
 
-    for nombre, numero in diccionario_deudores.items():
-        # Agrega el código de marcación para Colombia y Strip quita los espacios en blanco
-        numero_colombia = "+57" + numero.strip()
-
-        # Join se utiliza para concatenar cadenas y las separa
-        ruta_directorio = os.path.join('c:\\villa_fc\\directorio', f'{nombre}_{numero}')
+    try:
         
-        # makedirs crea directorios
-        # Si exist_ok=True, la función no generará un error si el directorio ya existe
-        os.makedirs(ruta_directorio, exist_ok=True)
+        for nombre, numero in diccionario_deudores.items():
+            # Agrega el código de marcación para Colombia y Strip quita los espacios en blanco del número
+            numero_colombia = "+57" + numero.strip()
 
-        mensaje = f'Hola {nombre}.\n\n🚩Recuerda tu cuota pendiente en Villa_FC.\n\nPor favor, realiza el pago pronto para evitar inconvenientes. ¡Gracias!'
-        hora_envio = datetime.now().hour
-        minuto_envio = datetime.now().minute + 1
+            mensaje = f'Hola {nombre}.\n\n🚩Recuerda tu cuota pendiente en Villa_FC.\n\nPor favor, realiza el pago pronto para evitar inconvenientes. ¡Gracias!'
+            hora_envio = datetime.now().hour
+            minuto_envio = datetime.now().minute + 1
 
-        # Corrige la función de envío de mensajes
-        kit.sendwhatmsg_instantly(numero_colombia, mensaje, hora_envio, minuto_envio)
+            # Envío de mensajes a WhatsApp
+            kit.sendwhatmsg_instantly(numero_colombia, mensaje, hora_envio, minuto_envio)
 
-        print(f"Diccionario creado: {ruta_directorio}, mensaje enviado a {nombre} al número {numero_colombia}")
+            print(f"Mensaje enviado a {nombre} al número {numero_colombia}")
 
-except Exception as e:
-    print(f"Ha ocurrido un error: {str(e)}")
+    except Exception as e:
+        print(f"Ha ocurrido un error: {str(e)}")
--- src/villa.py
+++ src/villa.py
@@ -1,6 +1,16 @@
-
 import constants
 import utils
+from message import enviar_mensajes
+
+diccionario_deudores = {
+            "Jacobo Montenegro Angel": "3233073789",
+            "Juan Jose Gomez": "3165326067",
+            "Daniel Carvajal Correa": "3193917279",
+            "Manuela Correa Lopez": "3185957386"
+        }
 
 if __name__ == "__main__":
     df = utils.read_excel(constants.EXCEL_PATH)
+
+    #Llama la función       
+    enviar_mensajes(diccionario_deudores)
