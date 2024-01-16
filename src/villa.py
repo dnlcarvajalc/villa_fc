00953fbd23bccb733b3dbeff01bd280acb962548
Daniela0308 <danielavillamizartapias@gmail.com>
2024-01-16 16:39:12 -0500

    comentario de prueba

--- src/villa.py
+++ src/villa.py
@@ -5,8 +5,7 @@ from message import enviar_mensajes
 diccionario_deudores = {
             "Jacobo Montenegro Angel": "3233073789",
             "Juan Jose Gomez": "3165326067",
-            "Daniel Carvajal Correa": "3193917279",
-            "Manuela Correa Lopez": "3185957386"
+            
         }
 
 if __name__ == "__main__":
