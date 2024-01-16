d1ec3e444ed001a85c5f5d9c2b0af90db807c9ed
Jacobo Montenegro Ángel <Jacobo Montenegro Angel>
2024-01-16 12:05:56 -0500

    Realice los cambios sujeridos y optimización del codigo y comentarios de funciones

--- src/cobros.py
+++ src/cobros.py
@@ -2,20 +2,33 @@ import pandas as pd
 import constants
 from datetime import datetime
 
-
-fecha = datetime.now()
-
-
 def conv_datetime (df):
+    """Esta Funcion convierte las fechas del df en tipo datetime
+
+    Args:
+        df (type): Data frame con toda la informacion de pagos
+
+    Returns:
+        type: Data frame con la fecha en formato año, mes, dia 
+    """
     df_new = pd.to_datetime(df['ULTIMO_PAGO'], format= constants.DATE_FORMAT)
     return df_new
 
+
 def diccionario_deudores (df_new):
+    """Funcion para crear un diccionario en el cual se guarden las personas cuyo ultimo 
+       pago sea superior a 30 dias  
+
+    Args:
+        df_new (_type_): data frame con la fecha convertida al formato año, mes, dia
+    """
     deudores = {}
     for index, cliente in df_new.iterrows():
         fecha_convertida = conv_datetime(cliente)
+        fecha = datetime.now()
         df_new.at[index, 'Diferencia'] = fecha_convertida - fecha
         if df_new.at[index, 'Diferencia'] < constants.LIMIT_DAYS:        
             deudores.update({cliente['NOMBRE']: cliente['NUMERO']})
-     
+    print(df_new) 
+    print(deudores)
 
